7559f0006db825dba51311f95e82ff7551fe8577
trholdridge <trholdridge@gmail.com>
2022-01-27 16:45:42 -0500

    old site clone

---
+++ main/environ lit/monsoon_functions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 09:24:46 2019
+
+@author: tulasiholdridge
+"""
+
+def test(a):
+    if a>0:
+        print("hello world")
